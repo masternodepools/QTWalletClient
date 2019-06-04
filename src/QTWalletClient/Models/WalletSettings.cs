775c97bcbd2a18500033b5d4fcfdb5faf9b8edb9
henke164 <henrik.mattsson@hotmail.com>
2019-06-04 18:06:24 +0200

    port is int

--- src/QTWalletClient/Models/WalletSettings.cs
+++ src/QTWalletClient/Models/WalletSettings.cs
@@ -3,7 +3,7 @@
     public class WalletSettings
     {
         public string Ip { get; set; }
-        public string Port { get; set; }
+        public int Port { get; set; }
         public string UserName { get; set; }
         public string Password { get; set; }
     }
