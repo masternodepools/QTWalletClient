ce468d0e9c406056bacaa236a05f09b0eb3383cc
henke164 <henrik.mattsson@hotmail.com>
2019-06-11 08:31:25 +0200

    Added coinname to settings

--- src/QTWalletClient/Models/WalletSettings.cs
+++ src/QTWalletClient/Models/WalletSettings.cs
@@ -6,5 +6,6 @@
         public int Port { get; set; }
         public string UserName { get; set; }
         public string Password { get; set; }
+        public string CoinName { get; set; }
     }
 }
